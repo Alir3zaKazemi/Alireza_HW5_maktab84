a88de6b59572f85fc5d909972f549f1d3f5c1e91
Alir3za <alireza2212kazemi@gmail.com>
2022-09-30 22:28:06 +0330

    Q4.js modified

---
+++ Q4.js
@@ -0,0 +1,15 @@
+let arr = [{ txt: "", top: 0, right: 0 }];
+
+const textContent = prompt("whats your text?");
+arr[0].txt = textContent;
+
+const topPixel = prompt("enter a num az distance to top (in pixels)");
+arr[0].top = topPixel;
+
+const rightPixel = prompt("enter a num az distance to right (in pixels)");
+arr[0].right = rightPixel;
+
+let span = document.getElementsByTagName("span")[0];
+span.style.top = arr[0].top + "%";
+span.style.right = arr[0].right + "%";
+span.textContent = `${arr[0].txt}`;
